2def3a3faf17e60966f9e35ef3858de30c2f2dd9
RizkyArinandaAR <60787297+RizkyArinandaAR@users.noreply.github.com>
2020-02-07 22:43:26 +0700

    Update config.sample.php
    Otomatis Mengatur alamat website phpmixbill, baik untuk local maupun remote dari jauh, pada server local di dalam mikrotik

--- system/config.sample.php
+++ system/config.sample.php
@@ -4,6 +4,6 @@ $db_port 		= "";   # Database Port. Keep it blank if you are un sure.
 $db_user 		= "root"; # Database Username
 $db_password 	= ""; # Database Password
 $db_name 		= "phpmixbill"; # Database Name
-define('APP_URL', 'http://localhost/phpmixbill'); # Application URL.
+define('APP_URL', 'http://' .$_SERVER['HTTP_HOST']. '/phpmixbill'); # Application URL.
 #Please include http and do not use trailing slash after the url. For example use in this format- http://www.example.com Or http://www.example.com/finance
 $_app_stage = 'Live'; # Do not change this
